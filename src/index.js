dfa3f17abc6dbd5ac900eee20c19607a2181979e
Alex-Sorokolit <sorokolital@gmail.com>
2022-10-14 19:02:30 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,103 +1,47 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import { fetchCountries } from './js/fetchCountries';
 // Поле пошуку _____________________________________________
 // зробити Debounce на інпуті 300ms
 const DEBOUNCE_DELAY = 300;
-
-
-
-
-// Функція fetchCountries(name) робить HTTP-запит ___________
-
-// Search by country full name
-// https://restcountries.com/v2/name/{name}?fullText=true
-// https://restcountries.com/v2/name/aruba?fullText=true
-
-
-// Фільтрація полів _________________________________________
-// Потрібні наступні властивості:
-// name.official - повна назва країни
-// capital - столиця
-// population - населення
-// flags.svg - посилання на зображення прапора
-// languages - масив мов
-
-// Інтерфейс _________________________________________________
-// Якщо більше 10 країн показати "Too many matches found. Please enter a more specific name."
-// з допомогою notiflix
-
-// Якщо бекенд повернув від 2-х до 10-и країн, під тестовим полем відображається список знайдених країн.
-// Кожен елемент списку складається з прапора та назви країни.
-
-// Якщо результат запиту - це масив з однією країною, в інтерфейсі відображається розмітка картки з даними про країну: 
-// прапор, назва, столиця, населення і мови.
-
-
-
-
-
-
-
 
 refs ={
  inputCountryName: document.querySelector('#search-box'),
  countryList: document.querySelector('.country-list'),
  country: document.querySelector('.country-info'),
 }
-// console.log(refs.inputCountryName);
-
-const BASE_URL = 'https://restcountries.com/v3.1/name/'
-
 
 refs.inputCountryName.addEventListener('input',
-debounce(onSerch, DEBOUNCE_DELAY));
+debounce(onSerchInput, DEBOUNCE_DELAY));
 // console.log(inputValue);
 
-// Коли поле пошуку пусте потрібно видалити розмітку
-function onSerch(event) {
- removeList();
-  removeCountry();
-  
-// Меторд trim() видаляє зайві пробіли
- const inputValue = event.target.value.trim().toLowerCase();
 
-fetchCountries(inputValue);
-}
-
-
-const options = '?fields=name,capital,population,flags,languages';
 const notifyOptions = {
   position: 'center-top',
   fontSize: '20px',
   showOnlyTheLastOne: true,
 };
 
-const fetchCountries = (countryName) => {
- // перевіряємо як виглядає наш рядок запиту
- console.log(`${BASE_URL}${countryName}${options}`);
- // робимо запит 
- fetch(`${BASE_URL}${countryName}${options}`)
- // в першому then ми отримуємо екземпляр класу response (технічну інформацію і проміс з даними)
- .then(response => {
-  // якщо response має статус відмінний від ok, то викликаємо помилку
-// якщо країни не існує то показати "Oops, there is no country with that name"
-   if (!response.ok) {
-    Notify.failure('Oops, there is no country with that name',notifyOptions);
-    throw new Error(response.status);
-   }
-   return response.json();
-   
- })
- // в другому then ми отримуємо дані
- .then(data => {
-  // createMarkup(data);
-   // Data handling
 
+// Функція зпускає fetchCountries і функції видалення розмітки
+function onSerchInput(event) {
+// Коли поле пошуку пусте потрібно видалити розмітку
+removeList();
+removeCountry();
+  
+// Меторд trim() видаляє зайві пробіли
+  const inputValue = event.target.value.trim().toLowerCase();
+
+// в другому then ми отримуємо дані
+fetchCountries(inputValue).then(data => {
+// Якщо результат запиту - це масив з однією країною, в інтерфейсі відображається розмітка картки з даними про країну: 
+// прапор, назва, столиця, населення і мови.
 if (data.length === 1){
  renderCountryMarcup(data);
- console.log('one country');
+  console.log('one country');
+// Якщо бекенд повернув від 2-х до 10-и країн, під тестовим полем відображається список знайдених країн.
+// Кожен елемент списку складається з прапора та назви країни.
 } else if (data.length >= 2 && data.length <= 10){
  console.log('list countries');
  renderListMarkup(data);
@@ -109,22 +53,7 @@ if (data.length === 1){
  .catch(error => {
  console.log(error);
  });
-}
-
-
-
-// function createMarkup(data){
-// const {capital, population, languages, flags, name} = data[0];
-// const languagesValue = Object.values(languages)
-// console.log(capital[0]);
-// console.log(population);
-// console.log(...languagesValue);
-// console.log(flags.svg);
-// console.log(name.official);
-// }
-
-
-
+};
 
  // Створення розмітки _________________________________________________________
 function createCountryMarkup({capital, population, languages, flags, name} ){
@@ -163,6 +92,9 @@ const languagesValue = Object.values(languages).join(', ')
       </li>`
  };
 
+
+ // Якщо більше 10 країн показати "Too many matches found. Please enter a more specific name."
+// з допомогою notiflix
 function manyCountries(countries) {
   Notify.info('Too many matches found. Please enter a more specific name.', notifyOptions);
 console.log("Too many matches found. Please enter a more specific name.");
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,29 @@
-// Функція fetchCountries(name) робить HTTP-запит
-// повертає проміс з масивом країн
-// функція повинна бути в окремому файлі fetchCountries.js
-// зробити іменований експорт.
+import { Notify } from "notiflix";
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
+const options = '?fields=name,capital,population,flags,languages';
+
+// Функція fetchCountries(name) робить HTTP-запит ___________
+export function fetchCountries (countryName) {
+ // перевіряємо як виглядає наш рядок запиту
+ // console.log(`${BASE_URL}${countryName}${options}`);
+ // робимо запит 
+ 
+ return fetch(`${BASE_URL}${countryName}${options}`)
+ // в першому then ми отримуємо екземпляр класу response (технічну інформацію і проміс з даними)
+ .then(response => {
+  // якщо response має статус відмінний від ok, то викликаємо помилку
+// якщо країни не існує то показати "Oops, there is no country with that name"
+   if (!response.ok) {
+    Notify.failure('Oops, there is no country with that name',{
+  position: 'center-top',
+  fontSize: '20px',
+  showOnlyTheLastOne: true,
+});
+    throw new Error(response.status);
+   }
+   return response.json();
+   
+ })
+}
 
